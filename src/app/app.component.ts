901092472e39e625e88ab7345043ed9319ac437f
leco <lcosta.sp.br@gmail.com>
2019-03-18 10:30:57 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'l3co-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'business-plan';
+}
